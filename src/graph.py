986414a90f3e54772f47dd95b6bd61f10ff47b1d
berlianasr <berlianasrlta@gmail.com>
2025-03-10 21:44:20 +0700

    graphmodul1

--- src/graph.py
+++ src/graph.py
@@ -24,3 +24,14 @@ def bfs_traversal(G: nx.Graph, start: int) -> list[int]:
 #5
 def find_shortest_path(G: nx.Graph, source: int, target: int) -> list[int]:
     return nx.shortest_path(G, source=source, target=target)
+
+#6
+import matplotlib.pyplot as plt
+
+def visualize_graph(G: nx.Graph) -> None:
+    plt.figure(figsize=(8, 6))
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True, node_color='skyblue', edge_color='gray', node_size=2000, font_size=12)
+    
+    plt.savefig("graph_visualization.png")
+    plt.show()
